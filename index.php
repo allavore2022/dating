78a3b98720b437e5ea123648874d6d6df3db9dfb
Alisa Llavore <allavore@mail.greenriver.edu>
2021-02-10 02:28:17 -0500

    Added session array

--- index.php
+++ index.php
@@ -30,19 +30,59 @@ $f3->route('GET /personalInformation', function () {
 });
 
 //Define a profile route
-$f3->route('GET /profile', function () {
+$f3->route('POST /profile', function () {
+    //add information from personalInformation to Session array
+    if(isset($_POST['fname'])){
+        $_SESSION['fname'] = $_POST['fname'];
+    }
+    if(isset($_POST['lname'])){
+        $_SESSION['lname'] = $_POST['lname'];
+    }
+    if(isset($_POST['gender'])){
+        $_SESSION['gender'] = $_POST['gender'];
+    }
+    if(isset($_POST['age'])){
+        $_SESSION['age'] = $_POST['age'];
+    }
+    if(isset($_POST['pnumber'])){
+        $_SESSION['pnumber'] = $_POST['pnumber'];
+    }
+
     $view = new Template();
     echo $view->render('views/profile.html');
 });
 
 //Define an interests route
-$f3->route('GET /interests', function () {
+$f3->route('POST /interests', function () {
+    //add information from profile to session array
+    if(isset($_POST['email'])){
+        $_SESSION['email'] = $_POST['email'];
+    }
+    if(isset($_POST['state'])){
+        $_SESSION['state'] = $_POST['state'];
+    }
+    if(isset($_POST['seeking'])){
+        $_SESSION['seeking'] = $_POST['seeking'];
+    }
+    if(isset($_POST['bio'])){
+        $_SESSION['bio'] = $_POST['bio'];
+    }
+
     $view = new Template();
     echo $view->render('views/interests.html');
 });
 
 //Define a summary route
 $f3->route('POST /summary', function () {
+
+    //add information from interest to summary
+    if(isset($_POST['indoorInterests'])) {
+        $_SESSION['indoorInterests'] = implode(", ", $_POST['indoorInterests']);
+    }
+    if(isset($_POST['outdoorInterests'])) {
+        $_SESSION['outdoorInterests'] = implode(", ", $_POST['outdoorInterests']);
+    }
+
     $view = new Template();
     echo $view->render('views/summary.html');
 });
